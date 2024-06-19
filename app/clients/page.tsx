d9d1b757fb1e45e12cd076da1b23855161cfbebf
Jacques Everiss <jacques.everiss@gmail.com>
2024-06-19 12:52:47 +0100

    feat: added pages and page template

---
+++ app/clients/page.tsx
@@ -0,0 +1,12 @@
+import styles from './pages.module.css'
+
+import React from 'react'
+
+type Props = {}
+
+const Clients = (props: Props) => {
+  return (
+    <div>Clients</div>
+  )
+}
+export default Clients
